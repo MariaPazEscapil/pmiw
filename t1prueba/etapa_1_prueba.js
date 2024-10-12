08b2b7df0490e302ee463ddd98f8784191e19e46
rocioaldereteramos12 <130263698+rocioaldereteramos12@users.noreply.github.com>
2024-10-12 20:00:23 -0300

    Update etapa_1_prueba.js

--- t1prueba/etapa_1_prueba.js
+++ t1prueba/etapa_1_prueba.js
@@ -8,6 +8,14 @@ let xd = 240;
 let wd = 150;
 let hd = 40;
 let yd = 360;
+let xSi = 150;
+let ySi = 300;
+let wSi = 100;
+let hSi = 40;
+let xNo = 390;
+let yNo = 300;
+let wNo = 100;
+let hNo = 40;
 let sonido;
 let guion = [];
 
@@ -35,24 +43,53 @@ function draw() {
   textAlign(CENTER, CENTER);  
   text(guion[estado], width / 2, height / 2);  
 
+  if (estado != 3) {
+    fill(255);
+    textSize(20); 
+    if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+      fill(0, 0, 0, 170);
+    } else {
+      noFill();
+    }
+    rect(x, y, w, h);
+    fill(255);
+    textAlign(CENTER);  
+    text("Siguiente", x + w / 2, y + h / 2);  
+  }
   fill(255);
   textSize(20); 
-  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
-    fill(0, 0, 0, 170);
-  } else {
-    noFill();
-  }
-  rect(x, y, w, h);
-  fill(255);
-  textAlign(CENTER);  
-  text("Siguiente", x + w / 2, y + h / 2);  
   if (mouseX > xd && mouseX < xd + wd && mouseY > yd && mouseY < yd + hd) {
     fill(0, 0, 0, 170);
   } else {
     noFill();
   }
   rect(xd, yd, wd, hd);
+  fill(255);
   text("Anterior", xd + wd / 2, yd + hd / 2); 
+
+  if (estado == 3) {
+   
+    fill(255);
+    if (mouseX > xSi && mouseX < xSi + wSi && mouseY > ySi && mouseY < ySi + hSi) {
+      fill(0, 0, 0, 170);
+    } else {
+      noFill();
+    }
+    rect(xSi, ySi, wSi, hSi);
+    fill(255);
+    text("Sí", xSi + wSi / 2, ySi + hSi / 2);
+
+    fill(255);
+    if (mouseX > xNo && mouseX < xNo + wNo && mouseY > yNo && mouseY < yNo + hNo) {
+      fill(0, 0, 0, 170);
+    } else {
+      noFill();
+    }
+    rect(xNo, yNo, wNo, hNo);
+    fill(255);
+    text("No", xNo + wNo / 2, yNo + hNo / 2);
+  }
+
   push();
   textSize(40);
   textAlign(CENTER);
@@ -61,14 +98,25 @@ function draw() {
 }
 
 function mousePressed() {
-  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+  if (estado != 3 && mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
     if (estado < imagenes.length - 1) {
       estado++;  
-    }
   }
+  }
+ 
   if (mouseX > xd && mouseX < xd + wd && mouseY > yd && mouseY < yd + hd) {
     if (estado > 0) {
       estado--; 
     }
   }
+  if (estado == 3 && mouseX > xSi && mouseX < xSi + wSi && mouseY > ySi && mouseY < ySi + hSi) {
+    estado = 6;
+  }
+  if (estado == 3 && mouseX > xNo && mouseX < xNo + wNo && mouseY > yNo && mouseY < yNo + hNo) {
+    estado = 5;
+  }
 }
+
+
+
+
