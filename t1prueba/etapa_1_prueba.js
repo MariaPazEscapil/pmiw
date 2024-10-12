0ca89a7c38f306004ac44c4abb8a55c06202f6fc
rocioaldereteramos12 <130263698+rocioaldereteramos12@users.noreply.github.com>
2024-10-12 17:25:05 -0300

    Update etapa_1_prueba.js

--- t1prueba/etapa_1_prueba.js
+++ t1prueba/etapa_1_prueba.js
@@ -1,54 +1,74 @@
-//Variables
 let imagenes = [];
-
-let estados;
-
-
+let estado = 0;  
 let x = 240;
 let y = 300;
 let w = 150;
 let h = 40;
-
 let xd = 240;
 let wd = 150;
 let hd = 40;
 let yd = 360;
+let sonido;
+let guion = [];
 
-function preload(){
-for (let i=0; i<2; i++) {
-    imagenes[i] = loadImage("data/joel_ellie"+i+".jpg");
+function preload() {
+  for (let i = 0; i < 6; i++) {
+    imagenes[i] = loadImage("data/imagen" + i + ".jpeg");  
   }
+
+  // sonido = loadSound("data/sound.mp3"); 
+  guion = loadStrings("data/guion.txt");  
 }
 
 function setup() {
-createCanvas(640, 480);
+  createCanvas(640, 480);
+  // sonido.loop(); 
 }
-
 
 function draw() {
-background(200);
-stroke(255);
+  background(200);
+  stroke(255);
+  
+  image(imagenes[estado], 0, 0, width, height);
+  fill(255);
+  textSize(24);  
+  textAlign(CENTER, CENTER);  
+  text(guion[estado], width / 2, height / 2);  
 
-if((mouseX > x) && (mouseX < x + w) && (mouseY > y) && (mouseY < y + h)){
-fill(0, 0, 0, 170);
-}else{
-  noFill();
+  fill(255);
+  textSize(20); 
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    fill(0, 0, 0, 170);
+  } else {
+    noFill();
+  }
+  rect(x, y, w, h);
+  fill(255);
+  textAlign(CENTER);  
+  text("Siguiente", x + w / 2, y + h / 2);  
+  if (mouseX > xd && mouseX < xd + wd && mouseY > yd && mouseY < yd + hd) {
+    fill(0, 0, 0, 170);
+  } else {
+    noFill();
+  }
+  rect(xd, yd, wd, hd);
+  text("Anterior", xd + wd / 2, yd + hd / 2); 
+  push();
+  textSize(40);
+  textAlign(CENTER);
+  text("The Last of Us", width / 2, 80);
+  pop();
 }
-rect(x,y,w,h);
 
-if((mouseX > xd) && (mouseX < xd + wd) && (mouseY > yd) && (mouseY < yd + hd)){
-fill(0, 0, 0, 170);
-}else{
-  noFill();
-}
-rect(xd,yd,wd,hd);
-
-fill(255);
-textSize(20);
-text("comenzar",271,325);
-text("creditos",280,387);
-push();
-textSize(40);
-text("The Last of Us",315,80);
-pop();
+function mousePressed() {
+  if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+    if (estado < imagenes.length - 1) {
+      estado++;  
+    }
+  }
+  if (mouseX > xd && mouseX < xd + wd && mouseY > yd && mouseY < yd + hd) {
+    if (estado > 0) {
+      estado--; 
+    }
+  }
 }
